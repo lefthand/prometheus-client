bcb17bbf06a69306467b1de1edc13dbf54c9270a
Eric Richardson <e@ericrichardson.com>
2015-03-16 15:59:06 -0400

    0.1.2: Fix port and service for haproxy exporter

--- resources/haproxy.rb
+++ resources/haproxy.rb
@@ -1,6 +1,6 @@
 actions :delete
 default_action :install
 
-attribute :port,        kind_of:String,   default:node.prometheus_client.node_exporter.port
-attribute :service,     kind_of:String,   default:node.prometheus_client.node_exporter.service
+attribute :port,        kind_of:String,   default:node.prometheus_client.haproxy_exporter.port
+attribute :service,     kind_of:String,   default:node.prometheus_client.haproxy_exporter.service
 attribute :scrape_uri,  kind_of:String,   required:true
